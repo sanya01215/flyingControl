5f88553404f36952de5d3c7f30db1c1c36f46f1e
sanya01215 <siabruk.o@gmail.com>
2022-01-13 14:25:44 +0200

    Initial commit

---
+++ src/main/java/com/workGroup/FlyingControlApp/FlyingControlAppApplication.java
@@ -0,0 +1,13 @@
+package com.workGroup.FlyingControlApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyingControlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyingControlAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workGroup/FlyingControlApp/FlyingControlAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workGroup.FlyingControlApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyingControlAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
